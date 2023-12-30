02cd7b8e2e582baa821e1804a15fc4d36759b158
Sukhmangill977 <sukhmangill977@gmail.com>
2023-12-30 23:28:44 +0530

    3 by sukhman

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,8 +4,11 @@ import heroImg01 from '../assets/images/hero-img01.png'
 import heroImg02 from '../assets/images/hero-img02.png'
 import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
+import icon02 from '../assets/images/icon02.png'
+import icon03 from '../assets/images/icon03.png'
 import { Link } from "react-router-dom"
-import {BsRightArrow} from "react-icons/bs"
+import {BsArrowRight} from "react-icons/bs"
+
 const Home = () => {
   return (
     <>
@@ -104,10 +107,57 @@ const Home = () => {
               expert health care. From the lab to the clinic.
             </p>
 
-            <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-
-            [#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor
-            hover:border-none">
-              <BsRightArrow className="group-hover:text-white w-6 h-5"/>
+            <Link 
+              to='/doctors'
+              className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px]
+              mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+              <BsArrowRight className="group-hover:text-white w-6 h-5"/>
+            
+            </Link>
+  
+              </div>
+            </div>
+            <div className='py-[30px] px-5'>
+            <div className='flex items-center justify-center'>
+              <img src={icon02} alt="" />
+            </div>
+          <div className='mt-[30px]'>
+            <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+              Find a Location
+            </h2>
+            <p className='text-[16px] leading-7 text-textcolor font-[400] mt-4 text-center'>
+              Word-class care for everyone. Our health System offers unmatched,
+              expert health care. From the lab to the clinic.
+            </p>
+
+            <Link 
+              to='/doctors'
+              className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px]
+              mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+              <BsArrowRight className="group-hover:text-white w-6 h-5"/>
+            
+            </Link>
+  
+              </div>
+            </div>
+            <div className='py-[30px] px-5'>
+            <div className='flex items-center justify-center'>
+              <img src={icon03} alt="" />
+            </div>
+          <div className='mt-[30px]'>
+            <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+              Book Appointment 
+            </h2>
+            <p className='text-[16px] leading-7 text-textcolor font-[400] mt-4 text-center'>
+              Word-class care for everyone. Our health System offers unmatched,
+              expert health care. From the lab to the clinic.
+            </p>
+
+            <Link 
+              to='/doctors'
+              className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px]
+              mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+              <BsArrowRight className="group-hover:text-white w-6 h-5"/>
             
             </Link>
   
@@ -116,7 +166,8 @@ const Home = () => {
           </div>
         </div>
       </section>
-      
+      <About/>
+     
     </>
   );
 };

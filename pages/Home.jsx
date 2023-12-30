07e4977f94f2ef5b9c1e1025a8781c223e2540c2
Sukhmangill977 <sukhmangill977@gmail.com>
2023-12-30 22:47:54 +0530

    2 by sukhman

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,6 +4,8 @@ import heroImg01 from '../assets/images/hero-img01.png'
 import heroImg02 from '../assets/images/hero-img02.png'
 import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
+import { Link } from "react-router-dom"
+import {BsRightArrow} from "react-icons/bs"
 const Home = () => {
   return (
     <>
@@ -83,15 +85,35 @@ const Home = () => {
             </h2>
             <p className='text__para text-center'>
             Word-class care for everyone. Our health System offers unmatched,
-            expert health.care
+            expert health care.
             </p>
           </div>
 
-       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px'></div>    
+       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
       
-      <div className='py-[39px] px-5'>
-        <div className='flex items-center justify-center'><img src={icon01} alt="" /></div>
-      </div>
+          <div className='py-[30px] px-5'>
+            <div className='flex items-center justify-center'>
+              <img src={icon01} alt="" />
+            </div>
+          <div className='mt-[30px]'>
+            <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+              Find a Doctor
+            </h2>
+            <p className='text-[16px] leading-7 text-textcolor font-[400] mt-4 text-center'>
+              Word-class care for everyone. Our health System offers unmatched,
+              expert health care. From the lab to the clinic.
+            </p>
+
+            <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-
+            [#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor
+            hover:border-none">
+              <BsRightArrow className="group-hover:text-white w-6 h-5"/>
+            
+            </Link>
+  
+              </div>
+            </div>
+          </div>
         </div>
       </section>
       
